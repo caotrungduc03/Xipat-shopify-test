6de111d1b2096117ecaf321938ea64b5d4812979
caotrungduc <trungdc2003@gmail.com>
2024-10-27 23:16:15 +0700

    feat: update products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,10 @@ import {
   Badge,
   Button,
   Card,
+  FormLayout,
   IndexTable,
+  Modal,
+  TextField,
   useIndexResourceState,
 } from "@shopify/polaris";
 import { PlusIcon } from "@shopify/polaris-icons";
@@ -13,6 +16,15 @@ const Products = () => {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
   const [products, setProducts] = useState([]);
+  const [isAddRuleModalOpen, setIsAddRuleModalOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isAddProductModalOpen, setIsAddProductModalOpen] = useState(false);
+  const [addProductFormData, setAddProductFormData] = useState({
+    title: "",
+    price: "",
+    image: null,
+    description: "",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -42,6 +54,48 @@ const Products = () => {
         setPage(page - 1);
       }
     }
+  };
+
+  const handleAddRuleModalOpen = (product) => {
+    setSelectedProduct(product);
+    setIsAddRuleModalOpen(true);
+  };
+
+  const handleAddRuleModalClose = () => {
+    setIsAddRuleModalOpen(false);
+    setSelectedProduct(null);
+  };
+
+  const handleAddProductModalOpen = () => {
+    setIsAddProductModalOpen(true);
+  };
+
+  const handleAddProductModalClose = () => {
+    setIsAddProductModalOpen(false);
+    setAddProductFormData({
+      title: "",
+      price: "",
+      image: null,
+      description: "",
+    });
+  };
+
+  const handleAddProductFormChange = (fieldName, value) => {
+    setAddProductFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleSaveProductModal = () => {
+    console.log(addProductFormData);
+    setIsAddProductModalOpen(false);
+    setAddProductFormData({
+      title: "",
+      price: "",
+      image: null,
+      description: "",
+    });
   };
 
   const startIndex = (page - 1) * limit;
@@ -76,7 +130,11 @@ const Products = () => {
           )}
         </IndexTable.Cell>
         <IndexTable.Cell>
-          <Button icon={PlusIcon} variant="primary">
+          <Button
+            icon={PlusIcon}
+            variant="primary"
+            onClick={() => handleAddRuleModalOpen({ title, rules })}
+          >
             Add rule
           </Button>
         </IndexTable.Cell>
@@ -108,7 +166,11 @@ const Products = () => {
             alignItems: "flex-end",
           }}
         >
-          <Button icon={PlusIcon} variant="primary">
+          <Button
+            icon={PlusIcon}
+            variant="primary"
+            onClick={handleAddProductModalOpen}
+          >
             Add Product
           </Button>
         </div>
@@ -141,6 +203,81 @@ const Products = () => {
           {rowMarkup}
         </IndexTable>
       </Card>
+      <Modal
+        open={isAddRuleModalOpen}
+        onClose={handleAddRuleModalClose}
+        title={`Detail rule`}
+      >
+        <Modal.Section>
+          <FormLayout>
+            <FormLayout.Group condensed>
+              <TextField
+                type="text"
+                label="Title"
+                value={selectedProduct?.title}
+              />
+              <TextField type="text" label="Start date" value={""} />
+              <TextField type="text" label="End date" value={""} />
+            </FormLayout.Group>
+            {selectedProduct?.rules.map((rule, index) => (
+              <FormLayout.Group key={index} condensed>
+                <TextField type="text" label="Buy from" value={rule.buyFrom} />
+                <TextField type="text" label="Buy to" value={rule.buyTo} />
+                <TextField
+                  type="number"
+                  label="Discount of item (%)"
+                  value={rule.discount}
+                />
+              </FormLayout.Group>
+            ))}
+          </FormLayout>
+        </Modal.Section>
+      </Modal>
+
+      <Modal
+        open={isAddProductModalOpen}
+        onClose={handleAddProductModalClose}
+        title={`Add product`}
+        primaryAction={{
+          content: "Save",
+          onAction: handleSaveProductModal,
+        }}
+        secondaryActions={[
+          {
+            content: "Cancel",
+            onAction: handleAddProductModalClose,
+          },
+        ]}
+      >
+        <Modal.Section>
+          <FormLayout>
+            <TextField
+              type="text"
+              label="Title"
+              value={addProductFormData?.title}
+              onChange={(e) => handleAddProductFormChange("title", e)}
+            />
+            <TextField
+              type="text"
+              label="Price"
+              value={addProductFormData.price}
+              onChange={(e) => handleAddProductFormChange("price", e)}
+            />
+            <TextField
+              type="file"
+              label="Image"
+              value={""}
+              onChange={(e) => handleAddProductFormChange("image", e)}
+            />
+            <TextField
+              type="text"
+              label="Description"
+              value={addProductFormData.description}
+              onChange={(e) => handleAddProductFormChange("description", e)}
+            />
+          </FormLayout>
+        </Modal.Section>
+      </Modal>
     </div>
   );
 };
