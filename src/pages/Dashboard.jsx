6ee6c9decd019dd59135ec464d6051ec06f14468
caotrungduc <trungdc2003@gmail.com>
2024-10-27 22:07:52 +0700

    feat: add products page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,37 +11,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-
-const calculateDate = (daysAgo) => {
-  const date = new Date();
-  date.setDate(date.getDate() - daysAgo);
-  return date.toISOString().split("T")[0];
-};
-
-const subscriptionData = [
-  { date: calculateDate(6), subscription: 120 },
-  { date: calculateDate(5), subscription: 280 },
-  { date: calculateDate(4), subscription: 120 },
-  { date: calculateDate(3), subscription: 200 },
-  { date: calculateDate(2), subscription: 220 },
-  { date: calculateDate(1), subscription: 150 },
-  { date: calculateDate(0), subscription: 280 },
-];
-
-const revenueData = [
-  { month: "Jan", revenue: 10000 },
-  { month: "Feb", revenue: 28000 },
-  { month: "Mar", revenue: 15000 },
-  { month: "Apr", revenue: 18000 },
-  { month: "May", revenue: 35000 },
-  { month: "Jun", revenue: 40000 },
-  { month: "Jul", revenue: 25000 },
-  { month: "Aug", revenue: 35000 },
-  { month: "Sep", revenue: 30000 },
-  { month: "Oct", revenue: 35000 },
-  { month: "Nov", revenue: 35000 },
-  { month: "Dec", revenue: 22000 },
-];
+import { calculateDate, revenueData, subscriptionData } from "../assets/data";
 
 const Dashboard = () => {
   const [startDate, setStartDate] = useState(calculateDate(6));
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,148 @@
+import {
+  Badge,
+  Button,
+  Card,
+  IndexTable,
+  useIndexResourceState,
+} from "@shopify/polaris";
+import { PlusIcon } from "@shopify/polaris-icons";
+import React, { useEffect, useState } from "react";
+import { productData } from "../assets/data";
+
 const Products = () => {
-  return <h1>Products</h1>;
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setProducts(productData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const { selectedResources, allResourcesSelected, handleSelectionChange } =
+    useIndexResourceState(products);
+
+  const handlePageChange = (type) => {
+    if (type === "next") {
+      if (page < Math.ceil(products.length / limit)) {
+        setPage(page + 1);
+      }
+    } else if (type === "previous") {
+      if (page > 1) {
+        setPage(page - 1);
+      }
+    }
+  };
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = startIndex + limit;
+  const visibleProducts = products.slice(startIndex, endIndex);
+
+  const rowMarkup = visibleProducts.map(
+    ({ id, image, title, rules, lastUpdate }, index) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <img
+            src={image}
+            alt={title}
+            style={{ width: "40px", height: "40px" }}
+          />
+        </IndexTable.Cell>
+        <IndexTable.Cell>{title}</IndexTable.Cell>
+        <IndexTable.Cell>{rules.length}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <span>{lastUpdate}</span>
+        </IndexTable.Cell>
+        <IndexTable.Cell>
+          {rules.length ? (
+            <Badge tone="success">Active</Badge>
+          ) : (
+            <Badge>No rule</Badge>
+          )}
+        </IndexTable.Cell>
+        <IndexTable.Cell>
+          <Button icon={PlusIcon} variant="primary">
+            Add rule
+          </Button>
+        </IndexTable.Cell>
+      </IndexTable.Row>
+    )
+  );
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "20px",
+            fontWeight: 600,
+            lineHeight: "24px",
+          }}
+        >
+          Products
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+          }}
+        >
+          <Button icon={PlusIcon} variant="primary">
+            Add Product
+          </Button>
+        </div>
+      </div>
+      <Card>
+        <IndexTable
+          itemCount={products.length}
+          selectedItemsCount={
+            allResourcesSelected ? "All" : selectedResources.length
+          }
+          onSelectionChange={handleSelectionChange}
+          headings={[
+            { title: "Image" },
+            { title: "Title product" },
+            { title: "Rules" },
+            { title: "Last update" },
+            { title: "Status" },
+            { title: "Action" },
+          ]}
+          pagination={{
+            label: `${page}-${Math.ceil(products.length / limit)} of ${
+              products.length
+            } products`,
+            hasPrevious: page > 1,
+            hasNext: page < Math.ceil(products.length / limit),
+            onPrevious: () => handlePageChange("previous"),
+            onNext: () => handlePageChange("next"),
+          }}
+        >
+          {rowMarkup}
+        </IndexTable>
+      </Card>
+    </div>
+  );
 };
 
 export default Products;
